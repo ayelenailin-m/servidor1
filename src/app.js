cc27fdce7da207232bc8aaec9709062f5a029121
ayelenailin-m <pukyailin@gmail.com>
2024-06-25 16:39:09 -0300

    .

--- src/app.js
+++ src/app.js
@@ -32,8 +32,33 @@ app.get("/categorias/:id", (req, res) => {
 app.post("/categorias", (req, res) => {
     const { id, categoria } = req.body;
     const newCaegoria = db.push({ id: id, categoria: categoria });
-    res.json({message: "Categoria creado con éxito"});
+    res.json({ message: "Categoria creado con éxito" });
 });
+
+//Editar una cateria
+app.put("/categorias/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { categoria } = req.body;
+    const getCategoria = db.find((categoria) => categoria.id === id);
+
+    getCategoria.id = id;
+
+    getCategoria.categoria = categoria;
+
+    res.json({ message: "Categoria actualizada" });
+});
+
+//Eliminar Categoria
+app.delete("/categorias/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const getCategoria = db.find((categoria) => categoria.id === id);
+    const categoriaIndex = db.indexOf(getCategoria);
+    const deletedCategoria = db.splice(categoriaIndex, 1);
+
+    res.json({ message: "Usuario eliminado", deletedCategoria });
+})
+
 
 
 //Corremos el servidor en el puerto
